5f7474837f838a7f4cb62f5de9aff3fed90360c1
nam710 <namanchawla029@gmail.com>
2024-10-12 13:17:17 +0530

    shadcn checked

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import { CardWithForm } from "@/components/CardWithForm";
+
+
 const HomePage = () => {
 	return (
 		<div className="bg-green-600 w-screen h-screen min-w-screen min-h-screen flex">
 			<div className="bg-red-600 w-1/12 min-h-screen "></div>
 			<div className="overflow-scroll no-scrollbar">
-				<section className="bg-blue-600 h-[500px] w-screen">Header</section>
+				<section className="bg-blue-600 h-[500px] w-screen">Header
+					<CardWithForm />
+				</section>
 				<section className="bg-blue-500 h-[500px] w-screen">About FGL</section>
 				<section className="bg-blue-400 h-[500px] w-screen">
 					Group Projects
